aca1c49970fd27119c9abbbcd1d734c2eedc50f6
Mr.Zhuang <725459159@qq.com>
2019-02-16 10:02:11 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,144 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <div style="text-align: right;font-size: 12px">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>查看</el-dropdown-item>
+              <el-dropdown-item>新增</el-dropdown-item>
+              <el-dropdown-item>删除</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span>王小虎</span>
+        </div>
+      </el-header>
+    </el-container>
+    <el-container style="height: 800px; border: 1px solid #eee">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu :default-openeds="['1', '3']" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>导航一</template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">
+                <router-link to="/">Home</router-link>
+              </el-menu-item>
+              <el-menu-item index="1-2">
+                <router-link to="/table-main">TableMain</router-link>
+              </el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="2-1">选项1</el-menu-item>
+              <el-menu-item index="2-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="2-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="3-1">选项1</el-menu-item>
+              <el-menu-item index="3-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="3-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="3-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-container>
+
+
+        <el-main>
+          <!--<el-table :data="tableData">-->
+          <!--<el-table-column prop="date" label="日期" width="140">-->
+          <!--</el-table-column>-->
+          <!--<el-table-column prop="name" label="姓名" width="120">-->
+          <!--</el-table-column>-->
+          <!--<el-table-column prop="address" label="地址">-->
+          <!--</el-table-column>-->
+          <!--</el-table>-->
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    data() {
+      const item = {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      };
+      return {
+        isCollapse: false,
+        tableData: Array(20).fill(item)
+      }
+    },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    },
+    name: 'app',
+    components: {
+      HelloWorld
+    }
+  }
+</script>
+
+<style>
+  /*#app {*/
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+  /*}*/
+  .el-header {
+    background-color: #409EFF;
+    color: #ffffff;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    color: #409EFF;
+  }
+
+  .el-footer {
+    background-color: #409EFF;
+  }
+</style>
